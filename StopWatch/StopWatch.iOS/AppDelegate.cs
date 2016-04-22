77157375a01ced01304611d0c99d6e46387dfec1
yuka1984 <newtamafuyou@gmail.com>
2016-04-22 18:43:38 +0900

    iOS Conrainer登録の追加

--- StopWatch/StopWatch.iOS/AppDelegate.cs
+++ StopWatch/StopWatch.iOS/AppDelegate.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
-
+using System.Runtime.InteropServices;
 using Foundation;
+using KeyValueStore.Pcl;
 using Microsoft.Practices.Unity;
+using SwLib.Core.Controller;
+using SwLib.Pcl.Controllers;
 using UIKit;
 
 namespace StopWatch.iOS
@@ -25,9 +29,76 @@ namespace StopWatch.iOS
         {
             global::Xamarin.Forms.Forms.Init();
             var container = new UnityContainer();
+
+            container.RegisterType<IStopWatchController, StopWatchController>(new ContainerControlledLifetimeManager());
+            container.RegisterType<IKeyValueStore, KeyValueStore.Core.Touch.KeyValueStore>(new ContainerControlledLifetimeManager());
+
             LoadApplication(new App(container));
 
             return base.FinishedLaunching(app, options);
+        }
+    }
+
+    public interface IHoge
+    {
+        void Start();
+        void Stop();
+    }
+
+    public abstract class HogeBase : IHoge
+    {
+        public static class NativeMethods
+        {
+            [DllImport("__Internal")]
+            public static extern void Init();
+
+            [DllImport("__Internal")]
+            public static extern void Start();
+
+            [DllImport("__Internal")]
+            public static extern void Stop();
+        }
+
+        public abstract void Start();
+        public abstract void Stop();
+    }
+
+    public class Hoge : HogeBase
+    {
+        private static Lazy<Hoge> _instance = new Lazy<Hoge>(() => new Hoge());
+        public static Hoge GetInstance() => _instance.Value;
+        private Hoge()
+        {
+            NativeMethods.Init();
+        }
+
+        public override void Start()
+        {
+            Debug.WriteLine("start");
+            NativeMethods.Start();
+        }
+
+        public override void Stop()
+        {
+            Debug.WriteLine("stop");
+            NativeMethods.Stop();
+        }
+    }
+    public class Hoge2 : IHoge
+    {
+        private static Lazy<Hoge> _instance = new Lazy<Hoge>(() => new Hoge());
+        public static Hoge GetInstance() => _instance.Value;
+        private Hoge2()
+        {
+            
+        }
+        public void Start()
+        {
+            Debug.WriteLine("start");            
+        }
+        public void Stop()
+        {
+            Debug.WriteLine("stop");
         }
     }
 }
